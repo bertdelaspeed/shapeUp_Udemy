f05968cf40f8114f316def7f4b7baca72eefa0c9
Wilfried Bertrand <bertdelaspeed@gmail.com>
2023-11-08 07:59:12 +0000

    Revert "firebase initialized"
    This reverts commit 0cbebe4a279a4d66ffed9d2514438faa8a89add5.

--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -6,24 +6,16 @@ import WorkoutOTD from "../Components/WorkoutOTD";
 import Separator from "../Components/Separator";
 import Category from "../Components/Category";
 import Exercise from "../Components/Exercise";
-import {
-  GestureHandlerRootView,
-  ScrollView,
-} from "react-native-gesture-handler";
 
 const WorkoutScreen = () => {
   return (
     <SafeAreaView className="mx-[1%]">
       <Welcome />
-      <GestureHandlerRootView>
-        <ScrollView>
-          <WorkoutOTD />
-          <Separator />
-          <Category />
-          <Separator />
-          <Exercise />
-        </ScrollView>
-      </GestureHandlerRootView>
+      <WorkoutOTD />
+      <Separator />
+      <Category />
+      <Separator />
+      <Exercise />
     </SafeAreaView>
   );
 };
